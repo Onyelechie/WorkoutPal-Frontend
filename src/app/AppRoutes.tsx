6c916341b74c31b2fb93a95388d99181ef7e2491
K2bit <k2b1t20@gmail.com>
2025-10-10 14:24:34 -0500

    Fixed missed refactors. Added more meaningful comments. Fixed Sunday bug in date.ts

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
-import LandingView from "../pages/LandingView";
+import Landing from "../pages/Landing";
 import PageNotFound from "../pages/PageNotFound";
-import RoutineBuilderView from "../pages/RoutineBuilder/RoutineBuilder";
+import RoutineBuilder from "../pages/RoutineBuilder/RoutineBuilder";
 
 export default function AppRoutes() {
   return (
@@ -10,8 +10,8 @@ export default function AppRoutes() {
       {/* Inspired from https://www.w3schools.com/react/react_router.asp */}
       <BrowserRouter>
         <Routes>
-          <Route path="/landing" element={<LandingView/>}/>
-          <Route path="/routine" element={<RoutineBuilderView/>}/>
+          <Route path="/landing" element={<Landing/>}/>
+          <Route path="/routine" element={<RoutineBuilder/>}/>
 
           {/* Direct to PageNotFound for routes that are not implemented */}
           <Route path="*" element={<PageNotFound/>}/>
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,7 +2,19 @@
 
 export const daysShortForm = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
 
+// returns the index of a day.
+// assumes an array has values Monday to Sunday in indices 0 to 6 respectively. i.e. Monday = 0, Wednesday = 2, Sunday = 6, etc.
+// only returns values from 0 to 6.
 export function getTodayIndex() {
     const today = new Date();
-    return today.getDay()-1; // -1 to offset for base 0
+
+    let dayIndex = today.getDay()-1; // -1 to offset for base 0
+    // today.getDay() will only return values from 0 to 6
+
+    if (dayIndex === -1) { // getDay() on a Sunday returns 0, so dayIndex = -1
+        const SUNDAY_INDEX = 6;
+        dayIndex = SUNDAY_INDEX; // go to Sunday
+    }
+
+    return dayIndex; 
 };
