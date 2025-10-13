d62eba3999a6c79ecd8ca78ff896cb6fe5d9be92
K2bit <k2b1t20@gmail.com>
2025-10-13 11:20:20 -0500

    CI Build fix

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,6 +1,4 @@
 import "./Dashboard.css";
-import Header from "../Header/Header.tsx";
-import Footer from "../Footer/Footer.tsx";
 import { notYetImplemented } from "../../utils/construction.ts";
 import { usePosts } from "../../hooks/usePosts.ts";
 
