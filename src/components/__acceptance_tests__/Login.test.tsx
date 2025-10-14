a0554710c0f9565f9f8df19b1b8757d2cbab8e36
javensamson <samsoncj@myumanitoba.ca>
2025-10-14 11:07:05 -0500

    Replace fireEvent with userEvent to simulate user interaction

--- src/components/__acceptance_tests__/Login.test.tsx
+++ src/components/__acceptance_tests__/Login.test.tsx
@@ -1,20 +1,47 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter, Routes, Route, } from 'react-router';
 import Login from '../../pages/Auth/Login';
-describe("Login acceptance test", () => {
+import Register from '../../pages/Auth/Register';
 
-  it("", () => {
+
+  it("must be able to register and login", async () => {
+    const user = userEvent.setup();
+    
     render(
-          <MemoryRouter initialEntries={['/auth/login']}>
-            <Routes>
-              <Route path='/auth/login' element={<Login />}/>
-            </Routes>
-          </MemoryRouter>
-    );
-    // screen.debug();
+      <MemoryRouter initialEntries={['/auth/login']}>
+        <Routes>
+          <Route path='/auth/login' element={<Login />}/>
+          <Route path='/auth/register' element={<Register />}/>
 
+        </Routes>
+      </MemoryRouter>
+    );
     expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument();
-    expect(true).toBe(true);    
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument();
+    
+    // https://testing-library.com/docs/user-event/intro
+    // navigate to Register
+    await user.click(screen.getByText("Create Account"));
+     
+    const inputName = screen.getByPlaceholderText("Name");
+    const inputUsername = screen.getByPlaceholderText(/username/i);
+    const inputEmail = screen.getByPlaceholderText(/email/i);
+    const inputPass = screen.getByPlaceholderText("Password");
+    const inputConfirmPass = screen.getByPlaceholderText("Confirm Password");
+
+    await user.type(inputName, "John");
+    await user.type(inputUsername, "John");
+    await user.type(inputEmail, "john@test.com");
+    await user.type(inputPass, "HelloWorld");
+    await user.type(inputConfirmPass, "HelloWorld");
+
+    const createAccButton = screen.getByRole("button", {name: /create account/i});
+    //must navigate back to Login
+    await user.click(createAccButton);
+    
+    // print current page
+    screen.debug();
+
   });
-});
