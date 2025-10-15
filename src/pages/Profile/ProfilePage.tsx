a96aa78d4510b506c388ce2ad46f8509fc3ceade
K2bit <k2b1t20@gmail.com>
2025-10-15 00:51:49 -0500

    Refactored for better readability

--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -10,38 +10,34 @@ function ProfilePage() {
 
   return (
     <div className="profile-page-container flex-row">
-      {isLoading ? (
-        "Loading..."
-      ) : error ? (
         <>
-          Failed to fetch user profile. Login?
-          <button onClick={navLogin}>Login</button>
-        </>
-      ) : user ? (
-        <>
-          <ProfileCard
-            avatar={user.avatar}
-            name={user.name}
-            username={user.username}
-            email={user.email}
-          />
-          <div className="stats-container">
-            <ul className="stats">
-              <li>Age: {user.age}</li>
-              <li>Height: {user.height}</li>
-              <li>Height &#40;Metric&#41; : {user.age}</li>
-              <li>Weight: {user.weight}</li>
-              <li>Weight &#40;Metric&#41; : {user.age}</li>
-            </ul>
-          </div>
+          {isLoading && <div>Loading user profile...</div>}
+          {error && <div>Failed to get user. Login? <button onClick={navLogin}>Login</button></div>}
+          {user && !isLoading && !error && (
+            <>
+              <ProfileCard
+              avatar={user.avatar}
+              name={user.name}
+              username={user.username}
+              email={user.email}
+            />
+            <div className="stats-container">
+              <ul className="stats">
+                <li>Age: {user.age}</li>
+                <li>Height: {user.height}</li>
+                <li>Height &#40;Metric&#41; : {user.age}</li>
+                <li>Weight: {user.weight}</li>
+                <li>Weight &#40;Metric&#41; : {user.age}</li>
+              </ul>
+            </div>
 
-          <div className="my-workouts-container">
-            <MyWorkouts/>
-          </div>
+            <div className="my-workouts-container">
+              <MyWorkouts/>
+            </div>
+          </>
+          )}
+          
         </>
-      ) : (
-        ""
-      )}
     </div>
   );
 }
