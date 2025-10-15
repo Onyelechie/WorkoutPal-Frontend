f2fe3b845e1b9bcfc6a81deadcee7bb050d9a89a
Taren Arns <arnst@myumanitoba.ca>
2025-10-14 22:01:35 -0500

    chore: naviagation changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,29 @@
 import '../styles/global.css';
-
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
-/* Wrap everything in the app with BrowserRouter to ensure page navigation works */
+function Layout() {
+  const location = useLocation();
+
+  const noHeader = ["/auth/login", "/auth/register"];
+
+  const hideHeader = noHeader.includes(location.pathname);
+
+  return (
+    <>
+      {!hideHeader && <Header />}
+      <AppRoutes />
+      <Footer />
+    </>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
-      {/* Apply the header to every single page */}
-      <Header/>
-
-      <AppRoutes/>
-
-      {/* Apply the footer to every single page */}
-      <Footer/>
+      <Layout />
     </BrowserRouter>
   );
 }
