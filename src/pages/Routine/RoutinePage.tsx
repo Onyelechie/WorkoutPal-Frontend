501332e42e2b45a23cbc15667a1790d6d7da9505
K2bit <k2b1t20@gmail.com>
2025-10-29 11:48:05 -0500

    Better mini nav in RoutinePage

--- src/pages/Routine/RoutinePage.tsx
+++ src/pages/Routine/RoutinePage.tsx
@@ -1,16 +1,31 @@
+import './RoutinePage.css';
 
-
-import { Outlet, useNavigate } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
+import { SCHEDULER_ROUTE, BUILDER_ROUTE } from "../../app/AppRoutes";
 
 export default function RoutinePage() {
 
     const navigate = useNavigate();
+    const location = useLocation();
 
     return (
     <>
         <div className="default-container">
-            <button onClick={() => {navigate("scheduler")}}> Go to scheduler </button>
-            <button onClick={() => {navigate("builder")}}> Go to builder </button>
+            <nav className="routine-nav-container">
+                <button
+                    className={`nav-button ${location.pathname.endsWith(SCHEDULER_ROUTE) ? "active" : ""}`}
+                    onClick={() => navigate(SCHEDULER_ROUTE)}
+                >
+                    Scheduler
+                </button>
+
+                <button
+                    className={`nav-button ${location.pathname.endsWith(BUILDER_ROUTE) ? "active" : ""}`}
+                    onClick={() => navigate(BUILDER_ROUTE)}
+                >
+                    Builder
+                </button>
+            </nav>
 
             {/* Outlet renders the matching child route of a parent route or nothing if no child route matches */}
             {/* https://reactrouter.com/api/components/Outlet */}
