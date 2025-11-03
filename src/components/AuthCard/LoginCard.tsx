7b2617ce51e74dd870ee1b460ebfb230c0444491
javensamson <samsoncj@myumanitoba.ca>
2025-11-03 17:46:47 -0600

    ctrl + z, rewind
    This reverts commit 014afd312f9f510acadd4d37d7e0aae1a13e7951.

--- src/components/AuthCard/LoginCard.tsx
+++ src/components/AuthCard/LoginCard.tsx
@@ -1,20 +1,15 @@
 import "./AuthCard.css";
 
 import React, { useState } from "react";
-import { getRequest, postRequest } from "../../utils/apiRequests";
+import { postRequest } from "../../utils/apiRequests";
 import { useAppNavigation } from "../../hooks/useAppNavigation";
 import { useAlertDialog } from "../../hooks/useAlertDialog";
-import { defaultAchievements, type AchievementTemplate } from "../../types/achievementTemplate";
-import { newUser } from "./CheckNewUser";
-import { postNewAchievement } from "../../services/achievementService";
-import { createAchievement } from "./UnlockLoginAchievement";
 
 function LoginCard() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { navHome, navRegister } = useAppNavigation();
   const dialogContext = useAlertDialog(); // access showAlert through context
-
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -26,25 +21,17 @@ function LoginCard() {
 
 
     postRequest("/login", payload)
-      .then(async (response) => {
+      .then((response) => {
         if (response.status == 200) {
           console.log("Login successful");
-          if (await newUser()) {
-            const achievement = createAchievement();
-            achievement.userId = 6;
-            if (await postNewAchievement(achievement)) {
-              dialogContext.showAlert(`Achievement Unlocked: ${achievement.badgeIcon} ${achievement.title}`, achievement.description);
-            }
-          }
-        } //
-        navHome();
-      }
-      )
+          navHome();
+        }
+      })
       .catch((error) => {
         if (error?.response) {
           console.error("Login failed:", error);
           dialogContext.showAlert("Login failed", "Please check your credentials and try again. " +
-            error.response.data
+              error.response.data
           );
         }
         else {
